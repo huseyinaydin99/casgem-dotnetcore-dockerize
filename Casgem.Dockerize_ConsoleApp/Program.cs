73ac6db28c5cda67b168044b63f893df135e97c3
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-03 10:48:36 +0300

    Add project files.

---
+++ Casgem.Dockerize_ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Selamun Aleyküm. | Merhabalar yuvarlak dünya.");
+int i = 1;
+while (i <= 10000)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine(i);
+    i++;
+}
